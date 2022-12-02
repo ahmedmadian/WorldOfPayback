6d2f8a41a714fe84dd0f9a4ea960c19b571d4f0e
Ahmed Madian <ahmed.madian@sengaro.com>
2022-12-02 22:57:17 +0100

    [Payback-1] init project

---
+++ WorldOfPayback/WorldOfPayback/Application/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  WorldOfPayback
+//
+//  Created by Ahmed Madian on 02.12.22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ WorldOfPayback/WorldOfPayback/ViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ViewController.swift
+//  WorldOfPayback
+//
+//  Created by Ahmed Madian on 02.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+}
