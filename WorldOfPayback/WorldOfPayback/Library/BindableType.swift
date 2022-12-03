5ae71973ed4ebcc4d378e6307f433224b10dd90a
Ahmed Madian <ahmed.mostafa.madian@gmail.com>
2022-12-03 14:55:00 +0100

    [Payback-8] add BindableType, a communication channel between View and its ViewModel

---
+++ WorldOfPayback/WorldOfPayback/Library/BindableType.swift
@@ -0,0 +1,30 @@
+//
+//  BindableType.swift
+//  WorldOfPayback
+//
+//  Created by Ahmed Madian on 03.12.22.
+//
+
+import UIKit
+
+protocol BindableType: AnyObject {
+    associatedtype ViewModelType
+
+    var viewModel: ViewModelType! { get set }
+
+    func bindViewModel()
+}
+
+extension BindableType where Self: UIViewController {
+    func bind(to model: Self.ViewModelType) {
+        viewModel = model
+        bindViewModel()
+    }
+}
+
+extension BindableType where Self: UITableViewCell {
+    func bind(to model: Self.ViewModelType) {
+        viewModel = model
+        bindViewModel()
+    }
+}
